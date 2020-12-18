f7a229650af192d646152378f7d93c3df232109a
amy-hyunji <alee6868@kaist.ac.kr>
2020-12-18 02:44:31 +0000

    fix(run_train.sh): change script to use dp

--- run_train.sh
+++ run_train.sh
@@ -1,3 +1,3 @@
-./prepare.sh
-pip install -r requirements.txt
-python train.py  --gradient_clip_val 1.0 --max_epochs 50 --default_root_dir logs  --gpus 1 --batch_size 2
+#./prepare.sh
+#pip install -r requirements.txt
+python train.py  --gradient_clip_val 1.0 --max_epochs 50 --default_root_dir logs  --accelerator 'dp' --log_gpu_memory 'all' --gpus 3 --batch_size 4 --weights_save_path "./ckpt"
